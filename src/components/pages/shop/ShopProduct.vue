dac3045291c62769d1060f45c9f2c8adaed02e7f
rajibcsit <18192203021@cse.bubt.edu.bd>
2022-09-05 16:36:38 +0600

    add cart & single product pages

---
+++ src/components/pages/shop/ShopProduct.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <h1 class="fleft">{{ singleProduct.name }} </h1>
+        <router-link to="/shop/products" class="fright"> All Products </router-link>
+        <div class="clear"></div>
+        <hr>
+        <div class="product-left">
+            <img :src="singleProduct.image" alt="" class="product_image single_p_img"/> 
+                <br><br>
+            <br>
+        </div>
+
+        <div class="product-right">
+            <div class="product-description">
+            <p>
+                <strong> Category: </strong> {{ singleProduct.category }} <br><br>
+                <strong> Supplier: </strong> {{ singleProduct.supplier }} <br><br>
+                <strong> Price: </strong> {{ singleProduct.price }} <br><br>
+                <strong> Description: </strong> {{ singleProduct.description }} <br><br>
+
+            </p>
+            <strong> Quantity: </strong>
+            <input type="number" class="input-number" v-model="qty">
+            <button class="addBtn" @click="addToCart()"> Add to Cart </button>
+        </div>
+
+        </div>
+        <div class="clear"></div>
+        <hr> <br><br><br>
+        <p class="txt-center">
+            <router-link to="/shop/products" class="addBtn"> Continue Shopping </router-link>
+        </p>
+
+    </div>
+  </template>
+  
+  <script>
+    import { Product } from '@/services/Product';
+    export default {
+      name: 'ShopProduct',
+      data (){
+        return{
+        // productId:this.$route.params.id,
+          singleProduct:{},
+            qty:1
+      }
+      },
+      mounted() {
+        Product.getProduct(this.$route.params.id)
+        .then((data) => {
+          this.singleProduct = data.data;
+        });
+     },
+   
+    }
+  </script>
